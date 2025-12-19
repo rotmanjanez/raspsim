979c069f98ded514f7bf2ad641a0e8a83600017b
Janez Rotman <janez@janez.at>
2025-12-19 19:11:34 +0100

    CI: switch runners to self-hosted for build and publish jobs
    python: update supported Python versions to 3.14 in classifiers and error message

--- pyraspsim/raspsim/__init__.py
+++ pyraspsim/raspsim/__init__.py
@@ -4,4 +4,4 @@ try:
     from .elf import *
 except ImportError as e:
     raise ImportError("""Failed to import core module. This is most likely due to an unsupoorted python version, plattform or architecture.
-    Supported versions are 3.7 to 3.12 on linux x86_64""") from e
+    Supported versions are 3.7 to 3.14 on linux x86_64""") from e
